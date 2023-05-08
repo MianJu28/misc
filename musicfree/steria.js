4ed3bf64be25fb617b5926a55970e128103def1e
MianJu28 <68192943+MianJu28@users.noreply.github.com>
2023-05-08 11:08:05 +0800

    Create steria.js

---
+++ musicfree/steria.js
@@ -0,0 +1,37 @@
+const axios = require("axios")
+
+async function search(query, page = 1, type) {
+    // 三个参数分别为: 查询的keyword，当前页码，搜索类型
+    if (type === 'music') {
+        // https://steria.vplayer.tk/api/musics/连名带姓/1
+        const data = (await axios.get('https://steria.vplayer.tk/api/musics/' + query + '/' + page)).data
+        const searchResults = []
+        const total = data.total
+        for (let music of data.data) {
+            searchResults.push({
+                id: music.id,
+                title: music.name,
+                artist: '薇Steria',
+                artwork: 'https://steria.vplayer.tk/static/images/steria.jpg',
+                url: music.url
+            })
+        }
+        return {
+            isEnd: page * 10 >= total,
+            data: searchResults
+        }
+    }
+    return {
+        isEnd: true, // 分页请求是否结束
+        data: [], // 不同type媒体类型的列表，即MusicItem[] | AlbumItem[] | ArtistItem[]
+    };
+}
+
+module.exports = {
+    platform: "薇Steria", // [必选] 插件名，搜索到的结果都会自动带上platform的标记
+    cacheControl: "cache", // [可选] 插件的缓存控制方案，用来缓存插件信息
+    version: "0.1.0", // [可选] 插件版本号
+    defaultSearchType: "music", // [可选] 插件在搜索时，首屏默认请求的搜索类型，默认是music。
+    /**[可选] 搜索 */
+    search
+}
