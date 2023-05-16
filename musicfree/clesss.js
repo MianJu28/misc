a9201e320615042d3706d9f378a168906366261c
MianJu28 <68192943+MianJu28@users.noreply.github.com>
2023-05-16 12:41:50 +0800

    Update clesss.js

--- musicfree/clesss.js
+++ musicfree/clesss.js
@@ -4,10 +4,9 @@ const cheerio = require('cheerio')
 module.exports = {
     platform: "ClessS", // [必选] 插件名，搜索到的结果都会自动带上platform的标记
     cacheControl: "cache", // [可选] 插件的缓存控制方案，用来缓存插件信息
-    version: "0.1.5", // [可选] 插件版本号
+    version: "0.1.6", // [可选] 插件版本号
     defaultSearchType: "music", // [可选] 插件在搜索时，首屏默认请求的搜索类型，默认是music。
     srcUrl: "https://raw.githubusercontent.com/MianJu28/misc/main/musicfree/clesss.js",
-    /**[可选] 搜索 */
     async getTopLists(){
         return [{
             title: "克勾播放器",
@@ -24,26 +23,15 @@ module.exports = {
         const musicList = []
         const $ = cheerio.load(rawHtml)
         const musics = JSON.parse($('#data').text())
-        // let id = 1
         for (let i = 0; i < musics.length; i++) {
             musicList.push({
                 id: i,
                 title: musics[i],
                 artist: '内德维德',
-                artwork: 'https://i2.hdslb.com/bfs/face/7aad26788e392df1b28ff21793abf7c9a990c6a1.jpg',
+                artwork: 'https://steria.vplayer.tk/static/images/steria.jpg',
                 url: 'http://clesss.xwbx.ink/audio/' + musics[i] + '.mp3'
             })
         }
-        // for (let music of musics) {
-        //     musics.push({
-        //         id: id,
-        //         title: music,
-        //         artist: '内德维德',
-        //         artwork: 'https://steria.vplayer.tk/static/images/steria.jpg',
-        //         url: 'http://clesss.xwbx.ink/audio/' + music
-        //     })
-        //     id++
-        // }
         return {
             id: topListItem.id,
             title: topListItem.title,
