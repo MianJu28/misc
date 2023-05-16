121e5c4594f57117b1f7e189ee3fd78bafb86528
MianJu28 <68192943+MianJu28@users.noreply.github.com>
2023-05-16 12:23:06 +0800

    Update clesss.js

--- musicfree/clesss.js
+++ musicfree/clesss.js
@@ -4,21 +4,11 @@ const cheerio = require('cheerio')
 module.exports = {
     platform: "ClessS", // [必选] 插件名，搜索到的结果都会自动带上platform的标记
     cacheControl: "cache", // [可选] 插件的缓存控制方案，用来缓存插件信息
-    version: "0.1.3", // [可选] 插件版本号
+    version: "0.1.4", // [可选] 插件版本号
     defaultSearchType: "music", // [可选] 插件在搜索时，首屏默认请求的搜索类型，默认是music。
     srcUrl: "https://raw.githubusercontent.com/MianJu28/misc/main/musicfree/clesss.js",
     /**[可选] 搜索 */
     async getTopLists(){
-        const data = (await axios.get('https://steria.vplayer.tk/api/tops')).data
-        const tops = []
-        for (let top of data) {
-            tops.push({
-                id: top.id,
-                description: '直播歌切',
-                coverImg: 'https://steria.vplayer.tk/static/images/steria.jpg',
-                title: top.title
-            })
-        }
         return [{
             title: "克勾播放器",
             data: [{
@@ -35,7 +25,7 @@ module.exports = {
         const $ = cheerio.load(rawHtml)
         const musics = JSON.parse($('#data').text())
         // let id = 1
-        for (let i = 0; i < 50; i++) {
+        for (let i = 0; i < 500; i++) {
             musicList.push({
                 id: i,
                 title: musics[i],
